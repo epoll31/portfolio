f68fa6341180add1eeadb3f2ddb1c13b5274fdcc
ethan <epollack31@gmail.com>
2024-05-31 16:36:47 -0700

    added custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+      <h1 className="text-6xl font-bold">404</h1>
+
+      <p className="text-xl">The page you are looking for does not exist.</p>
+    </div>
+  );
+}
