ae8cba866cb55c632afa7bbc1b948898d32f3c9d
ethan <epollack31@gmail.com>
2024-05-31 11:18:50 -0700

    better header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Github from "./icons/github";
 import Image from "next/image";
 import Links from "./links";
@@ -7,31 +6,37 @@ import Twitter from "./icons/twitter";
 
 export default function Header() {
   return (
-    <header className="bg-white border-b border-gray-200 grid grid-cols-[1fr_auto] pb-4 mb-4">
-      <h1 className="text-5xl font-bold self-end mb-4 mt-4">Ethan Pollack</h1>
-      <Image
-        src="/headshot.png"
-        width={75}
-        height={75}
-        alt="headshot"
-        className="rounded-xl col-start-2 row-span-2"
-      />
-      <Links
-        links={[
-          {
-            icon: <Github className="w-5 h-5" />,
-            href: "/github",
-          },
-          {
-            icon: <LinkedIn className="w-5 h-5" />,
-            href: "/linkedin",
-          },
-          {
-            icon: <Twitter className="w-5 h-5" />,
-            href: "/twitter",
-          },
-        ]}
-      />
+    <header className="bg-white border-b border-gray-200 flex justify-between gap-x-4 pb-4 mb-4">
+      <div className="flex flex-col justify-center gap-2">
+        <h1 className="text-3xl sm:text-5xl font-bold self-end ">
+          Ethan Pollack
+        </h1>
+        <Links
+          links={[
+            {
+              icon: <Github className="w-5 h-5" />,
+              href: "/github",
+            },
+            {
+              icon: <LinkedIn className="w-5 h-5" />,
+              href: "/linkedin",
+            },
+            {
+              icon: <Twitter className="w-5 h-5" />,
+              href: "/twitter",
+            },
+          ]}
+        />
+      </div>
+      <div className="flex items-center">
+        <Image
+          src="/headshot.png"
+          width={75}
+          height={75}
+          alt="headshot"
+          className="rounded-xl drop-shadow-xl"
+        />
+      </div>
     </header>
   );
 }
