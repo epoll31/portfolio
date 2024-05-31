cc2571cda2f2208d8b377189761cb468124ca29c
ethan <epollack31@gmail.com>
2024-05-31 15:24:26 -0700

    added blue shadow thing to header image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Github from "./icons/github";
 import Image from "next/image";
 import Links from "./links";
 import LinkedIn from "./icons/linkedin";
 import Twitter from "./icons/twitter";
 import MailFilled from "./icons/mail-filled";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
@@ -34,13 +37,31 @@ export default function Header() {
         />
       </div>
       <div className="flex items-center">
-        <Image
-          src="/headshot.png"
-          width={75}
-          height={75}
-          alt="headshot"
-          className="rounded-xl drop-shadow-xl"
-        />
+        <motion.div className="relative w-fit h-fit" whileHover={"hover"}>
+          <Image
+            src="/headshot.png"
+            width={75}
+            height={75}
+            alt="headshot"
+            className="rounded-xl drop-shadow-xl"
+          />
+          <motion.span
+            className="absolute top-[90%] left-0 w-full h-10 bg-gradient-to-br from-teal-400 to-teal-300 pointer-events-none"
+            style={{
+              maskImage: `radial-gradient(ellipse 100% 50% at 50% 50%, #00000050 0%, transparent 50%)`,
+            }}
+            initial={{
+              scaleX: 0.5,
+              scaleY: 0.5,
+            }}
+            variants={{
+              hover: {
+                scaleX: 1,
+                scaleY: 1,
+              },
+            }}
+          />
+        </motion.div>
       </div>
     </header>
   );
