03ccf742c24460cbc4faca5d70a0a75e6bff67f1
ethan <epollack31@gmail.com>
2024-05-31 23:58:56 -0700

    updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,9 +10,9 @@ import { motion } from "framer-motion";
 
 export default function Header() {
   return (
-    <header className="bg-white border-b border-gray-200 flex justify-between gap-x-4 pb-8 mb-8">
-      <div className="flex flex-col justify-center gap-2">
-        <h1 className="text-3xl sm:text-5xl font-bold self-end ">
+    <header className="bg-white border-b border-gray-200 flex justify-between mb-8">
+      <div className="flex flex-col justify-end items gap-2 pb-4 md:pb-8 ">
+        <h1 className="text-4xl sm:text-5xl font-bold self-end ">
           Ethan Pollack
         </h1>
         <Links
@@ -36,33 +36,42 @@ export default function Header() {
           ]}
         />
       </div>
-      <div className="flex items-center">
-        <motion.div className="relative w-fit h-fit" whileHover={"hover"}>
-          <Image
-            src="/headshot.png"
-            width={75}
-            height={75}
-            alt="headshot"
-            className="rounded-xl drop-shadow-xl"
-          />
-          <motion.span
-            className="absolute top-[90%] left-0 w-full h-10 bg-gradient-to-br from-teal-400 to-teal-300 pointer-events-none"
-            style={{
-              maskImage: `radial-gradient(ellipse 100% 50% at 50% 50%, #00000050 0%, transparent 50%)`,
-            }}
-            initial={{
-              scaleX: 0.5,
-              scaleY: 0.5,
-            }}
-            variants={{
-              hover: {
-                scaleX: 1,
-                scaleY: 1,
-              },
-            }}
-          />
-        </motion.div>
-      </div>
+      <motion.div className="relative w-fit h-fit" whileHover={"hover"}>
+        <Image
+          src="/headshot-no-bg.png"
+          width={150}
+          height={150}
+          alt="headshot"
+          className="rounded-xl drop-shadow-xl"
+          style={{
+            maskImage: "linear-gradient(to bottom, #000000 80%, #00000000)",
+          }}
+        />
+
+        {/* <Image
+          src="/ep-cartoon.png"
+          alt="Ethan Pollack"
+          width={50}
+          height={200}
+          className="drop-shadow-xl"
+        /> */}
+        <motion.span
+          className="absolute top-[90%] left-0 w-full h-10 bg-gradient-to-br from-teal-400 to-teal-300 pointer-events-none"
+          style={{
+            maskImage: `radial-gradient(ellipse 100% 50% at 50% 50%, #00000050 0%, transparent 50%)`,
+          }}
+          initial={{
+            scaleX: 0.5,
+            scaleY: 0.5,
+          }}
+          variants={{
+            hover: {
+              scaleX: 1,
+              scaleY: 1,
+            },
+          }}
+        />
+      </motion.div>
     </header>
   );
 }
