749a0fb739d4b57b321bd8a664b677f456b6dce1
ethan <epollack31@gmail.com>
2024-06-01 13:25:41 -0700

    updated header photo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,8 +10,8 @@ import { motion } from "framer-motion";
 
 export default function Header() {
   return (
-    <header className="bg-white border-b border-gray-200 flex justify-between mb-8">
-      <div className="flex flex-col justify-end items gap-2 pb-4 md:pb-8 ">
+    <header className="bg-white border-b border-gray-200 flex justify-between mb-8 pb-4">
+      <div className="flex flex-col justify-center items gap-2">
         <h1 className="text-4xl sm:text-5xl font-semibold self-end ">
           Ethan Pollack
         </h1>
@@ -37,30 +37,35 @@ export default function Header() {
         />
       </div>
       <motion.div className="relative w-fit h-fit" whileHover={"hover"}>
-        <Image
-          priority
-          src="/headshot-no-bg.png"
-          width={150}
-          height={150}
-          alt="headshot"
-          className="rounded-xl drop-shadow-xl"
-          style={{
-            maskImage: "linear-gradient(to bottom, #000000 80%, #00000000)",
+        <motion.div
+          variants={{
+            hover: {
+              translateY: -5,
+            },
           }}
-        />
+        >
+          <Image
+            priority
+            src="/grad-headshot-close.png"
+            width={100}
+            height={150}
+            alt="headshot"
+            className="rounded-lg drop-shadow-xl"
+          />
+        </motion.div>
         <motion.span
           className="absolute top-[90%] left-0 w-full h-10 bg-gradient-to-br from-teal-400 to-teal-300 pointer-events-none"
           style={{
             maskImage: `radial-gradient(ellipse 100% 50% at 50% 50%, #00000050 0%, transparent 50%)`,
           }}
           initial={{
-            scaleX: 0.5,
-            scaleY: 0.5,
+            scaleX: 0,
+            scaleY: 0,
           }}
           variants={{
             hover: {
-              scaleX: 1,
-              scaleY: 1,
+              scaleX: 1.5,
+              scaleY: 1.5,
             },
           }}
         />
