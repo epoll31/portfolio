d749927d682249a2a9f417ad7905d1622736f4bc
ethan <epollack31@gmail.com>
2024-06-01 00:17:13 -0700

    added image to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center w-full my-8">
+    <footer className="flex flex-col items-center  justify-center w-full my-8">
+      <Image
+        src="/headshot-no-bg.png"
+        width={50}
+        height={50}
+        alt="headshot"
+        style={{
+          maskImage: "linear-gradient(to bottom, #000000 90%, #00000000)",
+        }}
+      />
       <p className="text-sm font-light">© 2024 | Ethan Pollack</p>
     </footer>
   );
