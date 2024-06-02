d8c82582fd178512fbfe07498e97897147ff0426
ethan <epollack31@gmail.com>
2024-06-01 21:12:28 -0700

    added dark theme to project cards

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -122,7 +122,7 @@ function ProjectCard({ project }: { project: Project }) {
     <Link
       href={project.href}
       id={project.id}
-      className="rounded-xl border border-gray-200 hover:shadow-lg transition-shadow duration-300 overflow-hidden"
+      className="rounded-xl border border-gray-200 dark:border-neutral-600 hover:shadow-lg transition-shadow duration-300 overflow-hidden"
     >
       <GlowContainer
         whileHover
@@ -138,7 +138,7 @@ function ProjectCard({ project }: { project: Project }) {
           <div className="flex flex-wrap gap-2">
             {project.tags.map((tag) => (
               <span
-                className="bg-gray-50 text-gray-600 px-1.5 rounded-full text-xs font-light italic "
+                className="bg-gray-50 dark:bg-transparent text-gray-600 dark:text-gray-400 dark:border border-neutral-600 px-1.5 rounded-md text-xs font-light italic "
                 key={tag}
               >
                 {tag}
