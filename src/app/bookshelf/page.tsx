13288254128e475a386ecedea4461007c072bd92
ethan <epollack31@gmail.com>
2025-01-05 19:51:51 -0800

    added jurassic park

--- src/app/bookshelf/page.tsx
+++ src/app/bookshelf/page.tsx
@@ -6,179 +6,191 @@ const bookGroups: {
   size: "large" | "small" | "medium";
   books: BookInfo[];
 }[] = [
-  {
-    title: "Favorites",
-    size: "large",
-    books: [
-      {
-        title: "The Alchemist",
-        author: "Paulo Coelho",
-        cover: "/library/the-alchemist.png",
-        href: "https://www.goodreads.com/book/show/18144590-the-alchemist",
-      },
-      {
-        title: "Leonardo da Vinci",
-        author: "Walter Isaacson",
-        cover: "/library/leonardo-da-vinci.png",
-        href: "https://www.goodreads.com/book/show/34684622-leonardo-da-vinci",
-      },
-      {
-        title: "The Martian",
-        author: "Andy Weir",
-        cover: "/library/the-martian.png",
-        href: "https://www.goodreads.com/book/show/18007564-the-martian",
-      },
-      {
-        title: "Steve Jobs",
-        author: "Walter Isaacson",
-        cover: "/library/steve-jobs.png",
-        href: "https://www.goodreads.com/book/show/11084145-steve-jobs",
-      },
-      {
-        title: "Ender's Game",
-        author: "Orson Scott Card",
-        cover: "/library/enders-game.png",
-        href: "https://www.goodreads.com/book/show/375802.Ender_s_Game",
-      },
-      {
-        title: "Cosmos",
-        author: "Carl Sagan",
-        cover: "/library/cosmos.png",
-        href: "https://www.goodreads.com/book/show/55030.Cosmos",
-      },
-      {
-        title: "Project Hail Mary",
-        author: "Andy Weir",
-        cover: "/library/project-hail-mary.png",
-        href: "https://www.goodreads.com/book/show/54493401-project-hail-mary",
-      },
-    ],
-  },
-  {
-    title: "2024",
-    size: "medium",
-    books: [
-      {
-        title: "The Midnight Library",
-        author: "Matt Haig",
-        cover: "/library/the-midnight-library.jpg",
-        href: "https://www.goodreads.com/book/show/52578297-the-midnight-library",
-      },
-      {
-        title: "Wiseguy",
-        author: "Nicholas Pileggi",
-        cover: "/library/wiseguy.png",
-        href: "https://www.goodreads.com/book/show/158141.Wiseguy",
-      },
-      {
-        title: "Project Hail Mary",
-        author: "Andy Weir",
-        cover: "/library/project-hail-mary.png",
-        href: "https://www.goodreads.com/book/show/54493401-project-hail-mary",
-      },
-      {
-        title: "Clean Code",
-        author: "Robert C. Martin",
-        cover: "/library/clean-code.png",
-        href: "https://www.goodreads.com/book/show/3735293-clean-code",
-      },
-      {
-        title: "Ender's Game",
-        author: "Orson Scott Card",
-        cover: "/library/enders-game.png",
-        href: "https://www.goodreads.com/book/show/375802.Ender_s_Game",
-      },
-      {
-        title: "All Systems Red",
-        author: "Martha Wells",
-        cover: "/library/all-systems-red.png",
-        href: "https://www.goodreads.com/book/show/32758901-all-systems-red",
-      },
-      {
-        title: "Cosmos",
-        author: "Carl Sagan",
-        cover: "/library/cosmos.png",
-        href: "https://www.goodreads.com/book/show/55030.Cosmos",
-      },
-      {
-        title: "1491",
-        subTitle: "New Revelations of the Americas Before Columbus",
-        author: "Charles C. Mann",
-        cover: "/library/1491.png",
-        href: "https://www.goodreads.com/book/show/39020.1491",
-      },
-      {
-        title: "Conquering the Pacific",
-        subTitle:
-          "An Unknown Mariner and the Final Great Voyage of the Age of Discovery",
-        author: "Martin, Robert C.",
-        cover: "/library/conquering-the-pacific.png",
-        href: "https://www.goodreads.com/book/show/55959554-conquering-the-pacific",
-      },
-      {
-        title: "The Alchemist",
-        author: "Paulo Coelho",
-        cover: "/library/the-alchemist.png",
-        href: "https://www.goodreads.com/book/show/18144590-the-alchemist",
-      },
-    ],
-  },
-  {
-    title: "2023",
-    size: "small",
-    books: [
-      {
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        cover: "/library/the-hobbit.png",
-        href: "https://www.goodreads.com/book/show/5907.The_Hobbit",
-      },
-      {
-        title: "The Hidden Life of Trees",
-        author: "Peter Wohlleben",
-        cover: "/library/the-hidden-life-of-trees.png",
-        href: "https://www.goodreads.com/book/show/28256439-the-hidden-life-of-trees",
-      },
-      {
-        title: "Sleeping Giants",
-        author: "Sylvian Neuvel",
-        cover: "/library/sleeping-giants.png",
-        href: "https://www.goodreads.com/book/show/25733990-sleeping-giants",
-      },
-      {
-        title: "Outliers",
-        subTitle: "The Story of Success",
-        author: "Malcolm Gladwell",
-        cover: "/library/outliers.png",
-        href: "https://www.goodreads.com/book/show/3228917-outliers",
-      },
-      {
-        title: "The Power of Habit",
-        author: "Charles Duhigg",
-        cover: "/library/the-power-of-habit.png",
-        href: "https://www.goodreads.com/book/show/12609433-the-power-of-habit",
-      },
-      {
-        title: "Siddhartha",
-        author: "Hermann Hesse",
-        cover: "/library/siddhartha.png",
-        href: "https://www.goodreads.com/book/show/52036.Siddhartha",
-      },
-      {
-        title: "The Four Agreements",
-        author: "Don Miguel Ruiz",
-        cover: "/library/the-four-agreements.png",
-        href: "https://www.goodreads.com/book/show/6596.The_Four_Agreements",
-      },
-      {
-        title: "The Alchemist",
-        author: "Paulo Coelho",
-        cover: "/library/the-alchemist.png",
-        href: "https://www.goodreads.com/book/show/18144590-the-alchemist",
-      },
-    ],
-  },
-];
+    {
+      title: "Favorites",
+      size: "large",
+      books: [
+        {
+          title: "The Alchemist",
+          author: "Paulo Coelho",
+          cover: "/library/the-alchemist.png",
+          href: "https://www.goodreads.com/book/show/18144590-the-alchemist",
+        },
+        {
+          title: "Leonardo da Vinci",
+          author: "Walter Isaacson",
+          cover: "/library/leonardo-da-vinci.png",
+          href: "https://www.goodreads.com/book/show/34684622-leonardo-da-vinci",
+        },
+        {
+          title: "The Martian",
+          author: "Andy Weir",
+          cover: "/library/the-martian.png",
+          href: "https://www.goodreads.com/book/show/18007564-the-martian",
+        },
+        {
+          title: "Steve Jobs",
+          author: "Walter Isaacson",
+          cover: "/library/steve-jobs.png",
+          href: "https://www.goodreads.com/book/show/11084145-steve-jobs",
+        },
+        {
+          title: "Ender's Game",
+          author: "Orson Scott Card",
+          cover: "/library/enders-game.png",
+          href: "https://www.goodreads.com/book/show/375802.Ender_s_Game",
+        },
+        {
+          title: "Cosmos",
+          author: "Carl Sagan",
+          cover: "/library/cosmos.png",
+          href: "https://www.goodreads.com/book/show/55030.Cosmos",
+        },
+        {
+          title: "Project Hail Mary",
+          author: "Andy Weir",
+          cover: "/library/project-hail-mary.png",
+          href: "https://www.goodreads.com/book/show/54493401-project-hail-mary",
+        },
+        {
+          title: "Jurassic Park",
+          author: "Michael Chrighton",
+          cover: "/library/jurassic-park.jpg",
+          href: "https://www.goodreads.com/book/show/40604658-jurassic-park"
+        },
+      ],
+    },
+    {
+      title: "2024",
+      size: "medium",
+      books: [
+        {
+          title: "Jurassic Park",
+          author: "Michael Chrighton",
+          cover: "/library/jurassic-park.jpg",
+          href: "https://www.goodreads.com/book/show/40604658-jurassic-park"
+        },
+        {
+          title: "The Midnight Library",
+          author: "Matt Haig",
+          cover: "/library/the-midnight-library.jpg",
+          href: "https://www.goodreads.com/book/show/52578297-the-midnight-library",
+        },
+        {
+          title: "Wiseguy",
+          author: "Nicholas Pileggi",
+          cover: "/library/wiseguy.png",
+          href: "https://www.goodreads.com/book/show/158141.Wiseguy",
+        },
+        {
+          title: "Project Hail Mary",
+          author: "Andy Weir",
+          cover: "/library/project-hail-mary.png",
+          href: "https://www.goodreads.com/book/show/54493401-project-hail-mary",
+        },
+        {
+          title: "Clean Code",
+          author: "Robert C. Martin",
+          cover: "/library/clean-code.png",
+          href: "https://www.goodreads.com/book/show/3735293-clean-code",
+        },
+        {
+          title: "Ender's Game",
+          author: "Orson Scott Card",
+          cover: "/library/enders-game.png",
+          href: "https://www.goodreads.com/book/show/375802.Ender_s_Game",
+        },
+        {
+          title: "All Systems Red",
+          author: "Martha Wells",
+          cover: "/library/all-systems-red.png",
+          href: "https://www.goodreads.com/book/show/32758901-all-systems-red",
+        },
+        {
+          title: "Cosmos",
+          author: "Carl Sagan",
+          cover: "/library/cosmos.png",
+          href: "https://www.goodreads.com/book/show/55030.Cosmos",
+        },
+        {
+          title: "1491",
+          subTitle: "New Revelations of the Americas Before Columbus",
+          author: "Charles C. Mann",
+          cover: "/library/1491.png",
+          href: "https://www.goodreads.com/book/show/39020.1491",
+        },
+        {
+          title: "Conquering the Pacific",
+          subTitle:
+            "An Unknown Mariner and the Final Great Voyage of the Age of Discovery",
+          author: "Martin, Robert C.",
+          cover: "/library/conquering-the-pacific.png",
+          href: "https://www.goodreads.com/book/show/55959554-conquering-the-pacific",
+        },
+        {
+          title: "The Alchemist",
+          author: "Paulo Coelho",
+          cover: "/library/the-alchemist.png",
+          href: "https://www.goodreads.com/book/show/18144590-the-alchemist",
+        },
+      ],
+    },
+    {
+      title: "2023",
+      size: "small",
+      books: [
+        {
+          title: "The Hobbit",
+          author: "J.R.R. Tolkien",
+          cover: "/library/the-hobbit.png",
+          href: "https://www.goodreads.com/book/show/5907.The_Hobbit",
+        },
+        {
+          title: "The Hidden Life of Trees",
+          author: "Peter Wohlleben",
+          cover: "/library/the-hidden-life-of-trees.png",
+          href: "https://www.goodreads.com/book/show/28256439-the-hidden-life-of-trees",
+        },
+        {
+          title: "Sleeping Giants",
+          author: "Sylvian Neuvel",
+          cover: "/library/sleeping-giants.png",
+          href: "https://www.goodreads.com/book/show/25733990-sleeping-giants",
+        },
+        {
+          title: "Outliers",
+          subTitle: "The Story of Success",
+          author: "Malcolm Gladwell",
+          cover: "/library/outliers.png",
+          href: "https://www.goodreads.com/book/show/3228917-outliers",
+        },
+        {
+          title: "The Power of Habit",
+          author: "Charles Duhigg",
+          cover: "/library/the-power-of-habit.png",
+          href: "https://www.goodreads.com/book/show/12609433-the-power-of-habit",
+        },
+        {
+          title: "Siddhartha",
+          author: "Hermann Hesse",
+          cover: "/library/siddhartha.png",
+          href: "https://www.goodreads.com/book/show/52036.Siddhartha",
+        },
+        {
+          title: "The Four Agreements",
+          author: "Don Miguel Ruiz",
+          cover: "/library/the-four-agreements.png",
+          href: "https://www.goodreads.com/book/show/6596.The_Four_Agreements",
+        },
+        {
+          title: "The Alchemist",
+          author: "Paulo Coelho",
+          cover: "/library/the-alchemist.png",
+          href: "https://www.goodreads.com/book/show/18144590-the-alchemist",
+        },
+      ],
+    },
+  ];
 
 export default function BookshelfPage() {
   return (
