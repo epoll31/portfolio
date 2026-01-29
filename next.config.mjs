692ffe5a158a193c3ecf0adfa9255194dfaa29e7
ethan <epollack31@gmail.com>
2026-01-29 10:57:09 -0800

    testing ocd rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/ocd",
+  //       destination: "https://ocd-tracker-web-g0jtswrc9-epoll31s-projects.vercel.app",
+  //     },
+  //     {
+  //       source: "/ocd/:path*",
+  //       destination: "https://ocd-tracker-web-g0jtswrc9-epoll31s-projects.vercel.app/:path*",
+  //     },
+  //   ];
+  // },
+};
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -33,6 +33,11 @@ const redirects = [
   {
     from: "/peerlist",
     to: "https://peerlist.io/ethan",
+  },
+  {
+    from: "/ocd",
+    to: "https://ocd-tracker-web-g0jtswrc9-epoll31s-projects.vercel.app",
+    rewrite: true
   }
 ];
 
