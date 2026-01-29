79c446a5f27e8317331130916d5ed6fbdd2afc05
ethan <epollack31@gmail.com>
2026-01-29 11:09:14 -0800

    ocd rewrite test

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,11 @@ const nextConfig = {
     async rewrites() {
     return [
       {
-        source: "/ocd/:path*",
-        destination:
-          "https://ocd-tracker-web-g0jtswrc9-epoll31s-projects.vercel.app/:path*",
+        source: '/ocd',
+        destination: 'https://ocd-tracker-web-g0jtswrc9-epoll31s-projects.vercel.app',
       },
-    ];
+    ]
   },
-
 };
 
 export default nextConfig;
